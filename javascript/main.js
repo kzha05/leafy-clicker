5e283ce744e7e37b5e415cef1ac4fc9c6b03c156
diya jawalapersad <34486@ma-web.nl>
2023-04-04 11:39:17 +0200

    made tiny leaves

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,16 @@
-window.onload=function(){
-    var click=0;
-    document.getElementById("leaf").onclick=function(){
-        click=click + 1;
+window.onload = function(){
+    var click = 0;
+    document.getElementById("leaf").onclick = function(){
+        click = click + 1;
         console.log("leafy" + click);
-        document.getElementById("amount-of-leafs").innerHTML=click;
+        document.getElementById("amount-of-leafs").innerHTML = click;
+        var small_leaf = document.createElement("div");
+        small_leaf.className = "small-leaf"; 
+        document.body.appendChild(small_leaf);
+        var random_number = Math.floor(Math.random() * 100 + 1) //math.floor makes it even
+        small_leaf.style.left = random_number + "vw"; 
+        setTimeout(() => {
+            small_leaf.remove();
+        }, 1500 );
     }
 }
