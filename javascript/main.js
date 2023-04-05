6c38f475eafa3a377dbbfa472a2b4ed37bc9b68b
diya jawalapersad <34486@ma-web.nl>
2023-04-05 15:09:11 +0200

    added animation

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,34 @@
 window.onload = function(){
     var click = 0;
-    document.getElementById("leaf").onclick = function(){
-        click = click + 1;
-        console.log("leafy" + click);
-        document.getElementById("amount-of-leafs").innerHTML = click;
+
+    function small_leaf_animation(){
         var small_leaf = document.createElement("div");
         small_leaf.className = "small-leaf"; 
         document.body.appendChild(small_leaf);
         var random_number = Math.floor(Math.random() * 100 + 1) //math.floor makes it even
+        var random_number2 = Math.floor(Math.random() * 359 + 1) //math.floor makes it even
+        small_leaf.style.rotate = random_number2 + "deg"; 
         small_leaf.style.left = random_number + "vw"; 
         setTimeout(() => {
             small_leaf.remove();
         }, 1500 );
+    }
+
+    function add_leaf(){
+        click = click + 1;
+        document.getElementById("amount-of-leafs").innerHTML = click;
+    }
+
+    function big_leaf_animation(){
+        document.getElementById("leaf").className = "bigleaf-animation";
+        setTimeout(() => {
+            document.getElementById("leaf").className = "";
+        }, 350 );
+    }
+
+    document.getElementById("leaf").onclick = function(){
+       add_leaf();
+       small_leaf_animation();
+       big_leaf_animation();
     }
 }
